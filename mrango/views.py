ebf8a3cc6b1901bfc22bf6de5e985621a7ae2d4f
Cameron <cameroncairns@gmail.com>
2014-12-17 13:49:58 -0800

    Added a template to populate the index view for rango

--- mrango/views.py
+++ mrango/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there world! <br/><a href='/rango/about/'>About</a>")
+	#The context_dict is passed as an argument to the render function so that any django 
+	#template variable can be appropriately supplied
+	context_dict = { 'boldmessage' : "I am bold font from the context"}
+
+	#Render will give the user the modified template based on the context for the
+	#template variable which is supplied as a paramater
+	return render(request,'mrango/index.html', context_dict)
 def about(request):
 	return HttpResponse("Rango says here is the about page <br/><a href='/rango/'>Home</a>")
