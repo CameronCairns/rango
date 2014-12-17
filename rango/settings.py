8c577ef8bdd2bdf365537e9c3f70a2909df3429a
Cameron <cameroncairns@gmail.com>
2014-12-17 13:26:55 -0800

    Added templates directory and modified settings to accomodate this

--- rango/settings.py
+++ rango/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,7 +73,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
