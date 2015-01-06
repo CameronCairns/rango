3b7790cac336d6ef7016d44ef5804036ce3ff3ae
Cameron <cameroncairns@gmail.com>
2015-01-06 13:45:39 -0800

    Updated

--- mrango/views.py
+++ mrango/views.py
@@ -10,4 +10,10 @@ def index(request):
 	#template variable which is supplied as a paramater
 	return render(request,'mrango/index.html', context_dict)
 def about(request):
-	return HttpResponse("Rango says here is the about page <br/><a href='/rango/'>Home</a>")
+	#The context_dict is passed as an argument to the render function so that any django 
+	#template variable can be appropriately supplied
+	context_dict = { 'boldmessage' : "I am bold font from the about page"}
+
+	#Render will give the user the modified template based on the context for the
+	#template variable which is supplied as a paramater
+	return render(request,'mrango/about.html', context_dict)
